1106a07a8fba79a355d605641f9031c6ef9f940c
kenji <hotdrop.tkenji@gmail.com>
2023-05-03 12:15:08 +0900

    feat: トークン超過時に履歴を削る処理はrequestの方がいいと考えこちらに実装した

--- lib/data/remote/entities/gpt_request.dart
+++ lib/data/remote/entities/gpt_request.dart
@@ -4,13 +4,39 @@ import 'package:assistant_me/model/llm_model.dart';
 import 'package:assistant_me/model/talk.dart';
 
 class GptRequest {
-  GptRequest({
+  GptRequest._({
     required this.apiKey,
-    this.system,
+    required this.system,
     required this.newContents,
     required this.histories,
     required this.useModel,
   });
+
+  factory GptRequest.create({
+    required String apiKey,
+    required String? system,
+    required String message,
+    required List<Message> histories,
+    required LlmModel useModel,
+    int? overTokenNum,
+  }) {
+    if (overTokenNum == null) {
+      return GptRequest._(apiKey: apiKey, system: system, newContents: message, histories: histories, useModel: useModel);
+    }
+
+    // overTokenNumが指定されている場合はこっちを実行する
+    int ignoreTokenNum = overTokenNum - useModel.maxContext;
+    final adjustedHistories = <Message>[];
+    for (var history in histories) {
+      if (ignoreTokenNum >= 0) {
+        ignoreTokenNum = ignoreTokenNum - history.tokenNum;
+      } else {
+        adjustedHistories.add(history);
+      }
+    }
+
+    return GptRequest._(apiKey: apiKey, system: system, newContents: message, histories: adjustedHistories, useModel: useModel);
+  }
 
   final String apiKey;
   final String? system;
@@ -25,22 +51,8 @@ class GptRequest {
         'Content-type': 'application/json',
       };
 
-  String body({int? overLimitToken}) {
-    final historyTalks = <Map<String, String>>[];
-
-    if (overLimitToken == null) {
-      historyTalks.addAll(histories.map((h) => _toJsonContent(h)));
-    } else {
-      // overLimitTokenが設定されている場合はuseModelのMaxTokenとoverの差し引きで先頭から無視していく
-      int ignoreTokenNum = overLimitToken - useModel.maxContext;
-      for (var msg in histories) {
-        if (ignoreTokenNum >= 0) {
-          ignoreTokenNum = ignoreTokenNum - msg.tokenNum;
-        } else {
-          historyTalks.add(_toJsonContent(msg));
-        }
-      }
-    }
+  String body() {
+    final historyTalks = histories.map((h) => _toJsonContent(h));
 
     return json.encode({
       'model': useModel.name,
@@ -62,4 +74,7 @@ class GptRequest {
         throw UnimplementedError('未サポートのRoleTypeです index=${msg.roleType.index}');
     }
   }
+
+  // 履歴メッセージの合計トークン数を取得する
+  int get currentTotalTokenNum => histories.map((e) => e.tokenNum).fold(0, (prev, e) => prev + e);
 }
